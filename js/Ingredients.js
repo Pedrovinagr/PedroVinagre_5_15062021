faae51b27bc2bc92e38cc76f371511b59a5e7a9c
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-07-02 11:33:53 +0200

    create ingredients.js

---
+++ js/Ingredients.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log(recipes);
+
+    let ingredientsArray = [];
+    createIngredientsList(ingredientsArray);
+    console.log("Liste des ingredients");
+    console.log(ingredientsArray);
