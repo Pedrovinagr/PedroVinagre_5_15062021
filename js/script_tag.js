6f924a7b8543c991f81baeaf6a84ec25dcc47b18
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-07-05 14:11:48 +0200

    tag continued

--- js/script_tag.js
+++ js/script_tag.js
@@ -1,35 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log(recipes);
+//DOM
+const searchTagIngredients = document.querySelector('.search_ingredient');
+const tagIngredient = document.getElementById('tag_ingredient');
+const tagMotion = document.querySelector('.motion');
 
-    let ingredientsArray = [];
-    createIngredientsList(ingredientsArray);
-    console.log("Liste des ingredients");
-    console.log(ingredientsArray);
+tagIngredient.onkeyup = (e) =>{
+    let ingredientData = e.target.value;
+    let emptyArray = [];
 
-    let applianceArray = [];
-    createAppliancesList(applianceArray);
-    console.log("Liste des appareils");
-    console.log(applianceArray);
+    if(ingredientData){
+        emptyArray = ingredient.filter((data)=>{
+            return data.toLocaleLowerCase().startsWith(ingredientData.toLocaleLowerCase());
+        });
+        emptyArray = emptyArray.map((data)=>{
+            return data = '<li>'+ data +'<li>';
+        });
+        console.log(emptyArray);
+    }
+    showIngredients(emptyArray);
+}
 
-    let ustensilsArray = [];
-    createUstensilesList(ustensilsArray);
-    console.log("Liste des ustensiles");
-    console.log(ustensilsArray);
+function showIngredients(list){
+    let listData;
+    if(!list.length){
 
-    let searchBarInput = document.getElementById("searchbar");
-    searchBarInput.addEventListener("change", function(event) {
-        let searchText = event.target.value;
-        if(searchText !== "") {
-            if(ingredientsArray.indexOf(searchText) > -1) {
-                let indexOfIngredients = ingredientsArray.indexOf(searchText);
-                let ingredient = ingredientsArray[indexOfIngredients];
-                console.log("Ingredients visible dans notre tableau");
-                console.log(ingredient);
-                console.log("Recettes avec cet ingredient :");
-                let recipesToShow = searchRecipeFromIngredients(ingredient);
-                console.log(recipesToShow);
-                showRecipes(recipesToShow);
-            }
-        }
-    });
-});
+    }
+    else{
+        listData = list.join('');
+    }
+    tagMotion.innerHTML = listData;
+}
