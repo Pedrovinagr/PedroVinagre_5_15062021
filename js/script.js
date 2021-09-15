4b5784d584aed704e5fc69585cc9f2023b9755e8
Pedro Vinagre <pedro.vinagre@expleogroup.com>
2021-09-15 14:58:55 +0200

    création fichier variable globale

--- js/script.js
+++ js/script.js
@@ -1,38 +1,3 @@
-// VARIABLE GLOBAL
-var nameArray = [];
-var descriptionArray = [];
-var ingredientArray = [];
-var applianceArray = [];
-var ustensilArray = [];
-
-for (let i = 0; i < recipes.length; i++) {
-    var nameRecipe = recipes[i].name;
-    nameArray.push(nameRecipe);
-
-    var ingredientsRecipe = recipes[i].ingredients;
-    for (var j = 0; j < ingredientsRecipe.length; j++) {
-        var ingredientRecipe = ingredientsRecipe[j].ingredient;
-        ingredientArray.push(ingredientRecipe); 
-    }
-    
-    var descriptionRecipe = recipes[i].description;
-    descriptionArray.push(descriptionRecipe);
-
-    var applianceRecipe = recipes[i].appliance;
-    applianceArray.push(applianceRecipe);
-
-    var ustensilsRecipe = recipes[i].ustensils;
-    for (var j = 0; j < ustensilsRecipe.length; j++) {
-        var ustensilRecipe = ustensilsRecipe[j];
-        ustensilArray.push(ustensilRecipe); 
-    }
-    
-}
-console.log(nameArray)
-console.log(ingredientArray)
-console.log(descriptionArray)
-console.log(applianceArray)
-console.log(ustensilArray)
 // // BACIC DATA
 // var ingredientArrayDisplayInFilter = [];
 // var applianceArrayDisplayInFilter = [];
